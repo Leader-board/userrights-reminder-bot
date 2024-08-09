87e160b4698cade826f66193124c58a9ef728092
Leaderboard <leader-board@outlook.com>
2024-08-09 10:27:45 +0200

    fix name

--- reminder.py
+++ reminder.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 def get_users_expiry(wiki_name):
     cnx = mysql.connector.connect(option_files='replica.my.cnf', host=f'{wiki_name}.analytics.db.svc.wikimedia.cloud',
-                                  database=wiki_name)
+                                  database=f'{wiki_name}_p')
     query="""
     SELECT ug.ug_user, u.user_name, ug.ug_group, ug.ug_expiry from user_groups ug
     INNER JOIN user u
