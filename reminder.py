f0396755e33b06ef1757f3c9c91ee82825ece4a4
Leaderboard <leader-board@outlook.com>
2024-08-22 19:07:53 +0300

    no need to json that

--- reminder.py
+++ reminder.py
@@ -140,8 +140,7 @@ def get_json_dict(page_name, wiki_link = r'https://meta.wikimedia.org'):
     # print(f"page name = {page_name}")
     if 'error' in data_json:
         return None # does not exist
-    #print(data_json)
-    main_data = ast.literal_eval(data_json['parse']['wikitext']) # this is the actual JSON
+    main_data = data_json['parse']['wikitext'] # this is the actual JSON
 
     return main_data
 
